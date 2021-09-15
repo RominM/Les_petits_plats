0841a4ad6b18986e1e29129cccc7c918c333e158
RominM <rominmanogil.off@gmail.com>
2021-09-15 15:56:09 +0200

    css is in the place

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 import { HeaderDOM } from './js/Header.js';
 import { SearchSection } from './js/SearchSection.js';
+import { MainDOM } from './js/Main.js';
 
-fetch('./recipes.json') // le 'fetch' est une promesse mais ne donnera la reponse que lorsqu'il aura recupérer les données.
-// il n'empeche pas, cependant la lecture du fichier
-//ici, le fetch demande des information concernant le fichier 'data.json"
-	.then(result => { //ici sera decrit sa reponse
+fetch('./data.json')
+	.then(result => { 
 		return result.json();
-	}) //en attendant cette reponse, on lui demande d'initialiser la result2
+	})
 	.then(result2 => {
 		sessionStorage.setItem('data', JSON.stringify(result2));
 		init(result2);
+
 	});
 
-const init = (recipes) => {
-  console.log(recipes);
+const init = (data) => {
 
+	// data.recipes.forEach(recipe => {
+	// 	console.log(recipe);
+	// });
   const header = new HeaderDOM();
   header.buildHead();
   
   const searchSection = new SearchSection();
   searchSection.creatSearchBar();
   searchSection.sortByCards();
+
+    const main = new MainDOM(data);
+    main.buildMain();
 }
