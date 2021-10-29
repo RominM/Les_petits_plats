c7b5950cea6c939ceef722f52a2712def730ee7c
RominM <rominmanogil.off@gmail.com>
2021-10-29 18:58:07 +0200

    add appliance,ustensils in 'afterDeletedTag' function

--- js/afterDeleted.js
+++ js/afterDeleted.js
@@ -1,30 +1,48 @@
 import { recipes } from "./data.js";
-import { displayRecipes } from "./view/displayRecipes.js";
+// import { displayRecipes } from "./view/displayRecipes.js";
 
-export const afterDeletedTag = (tag) => {
-   const containsTags = document.querySelector('.contains-tags');
-   let STATEDATA = [...recipes];
+export const afterDeletedTag = (tag) => {// (tag) = tag cliqué/supprimer
+   const containsTags = document.querySelector('.contains-tags');// div parents contenant les div(ing, app, ust)
+   let STATEDATA = [...recipes];// data
 
-   for (let i = 0; i < containsTags.childNodes.length; i++) {
-      if (containsTags.childNodes[i].style.display !== 'none') {
+   for (let i = 0; i < containsTags.childNodes.length; i++) {// boucle sur les div(ing, app, ust) enfant de ".contains-tags"
+      if (containsTags.childNodes[i].style.display !== 'none') {// celles qui restent
          recipes.forEach(recipe => {
-
+            // INGREDIENTS
+            let ing;
             recipe.ingredients.forEach(ingredients => {
-               const ing = ingredients.ingredient;
-               containsTags.childNodes[i].childNodes.forEach(span => {
-                  const spanTag = span.innerHTML;
-                  
-                  if(ing.toLowerCase() === spanTag.toLowerCase()) {
-                     const registre = document.querySelector('.registre');
-                     registre.innerHTML = '';
-                     displayRecipes(STATEDATA)
-                  }
-               })
+               const ingArray = ingredients.ingredient;
+               ing = ingArray;
+            });
+
+            // APPAREIL
+            let app;
+            app = recipe.appliance;
+
+            // USTENSILS
+            let ust;
+            recipe.ustensils.forEach(ustensil => {
+               ust = ustensil;
             })
-         })
-      }
-   }
-}
+
+            // boucle sur les tags (dans les div (ing, app, ust))
+            containsTags.childNodes[i].childNodes.forEach(span => {
+               const spanTag = span.innerHTML;
+               if(ing.toLowerCase() === spanTag.toLowerCase()) {// comparer ingredient et tag (string). Si identique...
+                  //update en fonction des spanTag restants
+                  console.log(ing);
+               } else if (app.toLowerCase() === spanTag.toLowerCase()) {// comparer appareil et tag (string). Si identique...
+                  //update en fonction des spanTag restants
+                  console.log(app);
+               } else if (ust.toLowerCase() === spanTag.toLowerCase()) {// comparer ustensil et tag (string). Si identique...
+                  //update en fonction des spanTag restants
+                  console.log(ust);
+               }
+            });
+         });
+      };
+   };
+};
 
 // je veux UNIQUEMENT les tags restant dans la <div.contains-tags> après la suppression d'un tag === CHECK ! >>> containsTags.childNodes[i]
 // Je veux boucler sur recipe.ingredient. Si containsTags.childNodes[i] == recipe.ingredient, tu affiches les recettes
